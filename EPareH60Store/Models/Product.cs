4acd9d1e0e8990dcd814edf7d809d969eebe1b35
Paré <2368048@cegep-heritage.qc.ca>
2026-09-10 14:18:24 -0400

    Intialize DB

---
+++ EPareH60Store/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EPareH60Store.Models;
+
+public partial class Product
+{
+    public int ProductId { get; set; }
+
+    public int ProdCatId { get; set; }
+
+    public string? Description { get; set; }
+
+    public string? Manufacturer { get; set; }
+
+    public int Stock { get; set; }
+
+    public decimal? BuyPrice { get; set; }
+
+    public decimal? SellPrice { get; set; }
+
+    public virtual ProductCategory ProdCat { get; set; } = null!;
+}
