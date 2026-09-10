4be5b781931977071c965df0c7872de45773af76
Paré <2368048@cegep-heritage.qc.ca>
2026-09-10 14:50:21 -0400

    Add crud operations for product catergory

--- EPareH60Store/Models/ProductCategory.cs
+++ EPareH60Store/Models/ProductCategory.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 
 namespace EPareH60Store.Models;
@@ -10,4 +11,42 @@ public partial class ProductCategory
     public string ProdCat { get; set; } = null!;
 
     public virtual ICollection<Product> Products { get; set; } = new List<Product>();
-}
+
+
+
+
+        public static async Task AddCategoryAsync(H60assignmentDbEpContext context, ProductCategory category)
+        {
+            await context.ProductCategories.AddAsync(category);
+            await context.SaveChangesAsync();
+        }
+
+        public static async Task<IEnumerable<ProductCategory>> GetAllCategoriesAsync(H60assignmentDbEpContext context)
+        {
+            return await context.ProductCategories.ToListAsync();
+        }
+
+        public static async Task<ProductCategory?> GetCategoryByIdAsync(H60assignmentDbEpContext context, int id)
+        {
+            return await context.ProductCategories.FindAsync(id);
+        }
+
+        public static async Task UpdateCategoryAsync(H60assignmentDbEpContext context, ProductCategory category)
+        {
+            context.Entry(category).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+        }
+
+        
+        public static async Task DeleteCategoryAsync(H60assignmentDbEpContext context, int id)
+        {
+            var category = await context.ProductCategories.FindAsync(id);
+            if (category != null)
+            {
+                context.ProductCategories.Remove(category);
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+
+
