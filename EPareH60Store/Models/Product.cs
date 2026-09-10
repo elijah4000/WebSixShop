14c433aeaed18f456a23b59d53f4470a574b8210
Paré <2368048@cegep-heritage.qc.ca>
2026-09-10 14:32:56 -0400

    add crud for product.cs

--- EPareH60Store/Models/Product.cs
+++ EPareH60Store/Models/Product.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 
 namespace EPareH60Store.Models;
@@ -20,4 +21,42 @@ public partial class Product
     public decimal? SellPrice { get; set; }
 
     public virtual ProductCategory ProdCat { get; set; } = null!;
+
+
+
+      
+        public static async Task AddProductAsync(H60assignmentDbEpContext context, Product product)
+    {
+        await context.Products.AddAsync(product);
+        await context.SaveChangesAsync();
+    }
+
+    public static async Task<IEnumerable<Product>> GetAllProductsAsync(H60assignmentDbEpContext context)
+    {
+        return await context.Products.Include(p => p.ProductCategory).ToListAsync();
+    }
+
+    public static async Task<Product?> GetProductByIdAsync(H60assignmentDbEpContext context, int id)
+    {
+        return await context.Products.Include(p => p.ProductCategory)
+                                     .FirstOrDefaultAsync(p => p.ProductId == id);
+    }
+
+    public static async Task UpdateProductAsync(H60assignmentDbEpContext context, Product product)
+    {
+        context.Entry(product).State = EntityState.Modified;
+        await context.SaveChangesAsync();
+    }
+
+    public static async Task DeleteProductAsync(H60assignmentDbEpContext context, int id)
+    {
+        var product = await context.Products.FindAsync(id);
+        if (product != null)
+        {
+            context.Products.Remove(product);
+            await context.SaveChangesAsync();
+        }
+    }
 }
+    
+
