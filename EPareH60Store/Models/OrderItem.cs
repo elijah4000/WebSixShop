59f5f2ab1f01bce4f210fa7443a8748d502e5e1f
Elijah pare <pare.elijah@cegep-heritage.qc.ca>
2026-09-13 17:14:24 -0400

    configure order item model

---
+++ EPareH60Store/Models/OrderItem.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EPareH60Store.Models
+{
+    public class OrderItem
+    {
+        [Key]
+        public int OrderItemId { get; set; }
+
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+
+        [Column(TypeName = "numeric(8, 2)")]
+        public decimal Price { get; set; }
+
+        [ForeignKey("OrderId")]
+        public virtual Order Order { get; set; } = null!;
+
+        [ForeignKey("ProductId")]
+        public virtual Product Product { get; set; } = null!;
+    }
+}
